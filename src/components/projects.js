cd2475224a4339176c919ccbdd0db533c645d238
radhechaudhary <radhechaudhary6398@gmai.com>
2025-02-24 13:51:27 +0530

    ss'

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import slider from './slider.png'
 import keeper from './keeper-app.png'
-import regester from './regester-app.png'
+import regester from './register-app.png'
 import chat from './chatpage.png'
 import "aos/dist/aos.css";
 
@@ -16,7 +16,7 @@ function Projects(props) {
       <div className='projects-list' >
         <div data-aos="fade-down" data-aos-easing="linear"  className='project'    >
             <a href="https://regester-app-frontend.vercel.app/"><img src={regester}/></a>
-            <h3>Regester</h3>
+            <h3>Hotel Management</h3>
         </div>
         <div className='project' data-aos="fade-down" data-aos-easing="linear" data-aos-delay="150"  >
             <a href="https://keeperappproject.vercel.app/"><img src={keeper} /></a>
